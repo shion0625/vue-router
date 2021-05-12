2cdf3c0c3088e8d5e329621effbdf626c7fd48ad
shion0625 <kaito.yodogawa19@kit.edu.kh>
2021-05-13 01:15:38 +0900

    first commit

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -13,8 +13,23 @@
       exact
       class="link"
     >Users</router-link>
+    <button @click="increment()"> +1</button>
+    <button @click="decrement()"> -1</button>
   </nav>
 </template>
+<script>
+
+export default {
+  methods: {
+    increment(){
+      this.$store.dispatch('count/increment', 2)
+    },
+    decrement() {
+      this.$store.dispatch('count/decrement', 2)
+    }
+  }
+}
+</script>
 
 <style scoped>
 .link {
